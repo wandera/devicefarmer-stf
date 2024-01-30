c0d45afc68d5e564a4f9552882d1d3026975b648
erik.sedliak <erik.sedliak@wandera.com>
2024-01-30 13:42:45 +0100

    JTAND-9539 Add TestDPC shortcut 2

--- res/app/control-panes/dashboard/apps/apps-controller.js
+++ res/app/control-panes/dashboard/apps/apps-controller.js
@@ -63,6 +63,10 @@ module.exports = function ShellCtrl($scope) {
   $scope.openAgent = function() {
     run('am start com.airwatch.androidagent/com.airwatch.agent.ui.activity.SplashActivity')
   }
+
+  $scope.openTestDpc = function() {
+    run('am start com.afwsamples.testdpc/.PolicyManagementActivity')
+  }
   // WANDERA CUSTOM - END
 
   $scope.openLocaleSettings = function() {
